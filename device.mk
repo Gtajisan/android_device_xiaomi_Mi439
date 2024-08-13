5d2642f6637b1aa53535763b1b50d9a29fc1466b
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-13 23:18:18 +0600

    Update device.mk

--- device.mk
+++ device.mk
@@ -125,6 +125,9 @@ $(call inherit-product, vendor/xiaomi/Mi439/Mi439-vendor.mk)
 # Inherit viper4androidfx
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
+# Signed Build
+$(call inherit-product, vendor/extra/product.mk)
+
 # EXTRA: MiuiCamera
 ifneq ($(wildcard vendor/miuicamera/config.mk),)
 $(call inherit-product, vendor/miuicamera/config.mk)
