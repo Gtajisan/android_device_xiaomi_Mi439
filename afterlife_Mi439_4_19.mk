ed41658b8666371e57aedf095447a1e9146dc349
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-14 17:56:17 +0600

    Update afterlife_Mi439_4_19.mk

--- afterlife_Mi439_4_19.mk
+++ afterlife_Mi439_4_19.mk
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
+ROM_FOLDER := afterlife
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -33,7 +33,7 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := false
 TARGET_SUPPORTS_TOUCHGESTURES := true
-ROM_FOLDER := afterlife
+
 
 
 # Device identifier. This must come after all inclusions
