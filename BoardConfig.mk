525f4fbefd1b98874e4030f4b9a0cbc292942c2d
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-19 01:19:52 +0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,7 @@ $(foreach p, $(call to-upper, $(TREBLE_PARTITIONS)), \
 
 ifneq ($(WITH_GMS),true)
 BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 1287651328 # 1228 MB
+BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 314572800 # 300 MB
 endif
 
 # Power
@@ -160,3 +161,6 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Inherit from the proprietary version
 include vendor/xiaomi/Mi439/BoardConfigVendor.mk
+
+# Missing Required Module
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
