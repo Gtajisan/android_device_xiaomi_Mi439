2ff00c65876ce33c28618632d49fcaa3517d02e7
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-22 20:30:43 +0600

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-echo "Completed, proceeding to lunch"
-git clone https://gitlab.com/ItzDFPlayer/vendor_xiaomi_miuicamera -b leica-5.0-davinci vendor/xiaomi/miuicamera
-echo "Completed, proceeding to lunch"
